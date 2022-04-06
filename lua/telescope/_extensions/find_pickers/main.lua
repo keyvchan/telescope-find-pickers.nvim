166ad6de6bd0ed8411636cb010689257fd48319a
Keyv Chan <keyvchan@gmail.com>
2022-04-06 09:02:22 +0800

    feat: pass bufnr and winnr to lsp pickers (#4)
    * feat: pass bufnr and winnr to lsp pickers
    
    * chore: cleanup and use lua function to open pickers
    
    * fix: use vim.api instead of vim.fn

--- lua/telescope/_extensions/find_pickers/main.lua
+++ lua/telescope/_extensions/find_pickers/main.lua
@@ -5,14 +5,17 @@ local finders = require("telescope.finders")
 local actions_state = require("telescope.actions.state")
 local actions = require("telescope.actions")
 local themes = require("telescope.themes")
+local builtin_pickers = require("telescope.builtin")
+local extensions_pickers = require("telescope._extensions")
+
 -- telescope-project modules
 
 local M = {}
 
 -- Variables that setup can change
 local result_table = {}
-local builtin_list = vim.tbl_keys(require("telescope.builtin"))
-local extensions_list = vim.tbl_keys(require("telescope._extensions").manager)
+local builtin_list = vim.tbl_keys(builtin_pickers)
+local extensions_list = vim.tbl_keys(extensions_pickers.manager)
 
 for i, item in ipairs(builtin_list) do
 	table.insert(result_table, i, item)
@@ -21,11 +24,17 @@ for i, item in ipairs(extensions_list) do
 	table.insert(result_table, i, item)
 end
 
+local current_bufnr = vim.api.nvim_get_current_buf()
+local current_winnr = vim.api.nvim_get_current_win()
+
 M.setup = function(setup_config) end
 
 -- This creates a picker with a list of all of the pickers
 M.find_pickers = function(opts)
-	opts = opts or themes.get_dropdown()
+	opts = opts or themes.get_dropdown({
+		bufnr = current_bufnr,
+		winnr = current_winnr,
+	})
 
 	pickers.new(opts or {}, {
 		prompt_title = "Find Pickers",
@@ -38,8 +47,11 @@ M.find_pickers = function(opts)
 				local selection = actions_state.get_selected_entry()
 				local value = selection.value
 
-				-- TODO: Initial mode doesn't do anything, don't know why either
-				vim.api.nvim_command("Telescope " .. value .. " " .. "initial_mode=insert")
+				if builtin_pickers[value] ~= nil then
+					builtin_pickers[value](opts)
+				elseif extensions_pickers.manager[value] ~= nil then
+					extensions_pickers.manager[value](opts)
+				end
 			end)
 			return true
 		end,
