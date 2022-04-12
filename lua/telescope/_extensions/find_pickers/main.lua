1e59dcf8b44564a2bafd8629840109bea0965c59
Keyv Chan <keyvchan@gmail.com>
2022-04-12 19:20:19 +0800

    fix: find all pickers in extensions correctly, closed #5 (#7)
    * fix: pass bufnr and winnr to pickers
    
    * fix: get pickers in callback function
    
    * fix: close current picker before opening another

--- lua/telescope/_extensions/find_pickers/main.lua
+++ lua/telescope/_extensions/find_pickers/main.lua
@@ -12,31 +12,30 @@ local extensions_pickers = require("telescope._extensions")
 
 local M = {}
 
--- Variables that setup can change
-local result_table = {}
-local builtin_list = vim.tbl_keys(builtin_pickers)
-local extensions_list = vim.tbl_keys(extensions_pickers.manager)
-
-for i, item in ipairs(builtin_list) do
-	table.insert(result_table, i, item)
-end
-for i, item in ipairs(extensions_list) do
-	table.insert(result_table, i, item)
-end
-
-local current_bufnr = vim.api.nvim_get_current_buf()
-local current_winnr = vim.api.nvim_get_current_win()
-
 M.setup = function(setup_config) end
 
 -- This creates a picker with a list of all of the pickers
 M.find_pickers = function(opts)
-	opts = opts or themes.get_dropdown({
-		bufnr = current_bufnr,
-		winnr = current_winnr,
-	})
+	local opts_find_pickers = opts or themes.get_dropdown(opts)
 
-	pickers.new(opts or {}, {
+	local opts_pickers = {
+		bufnr = vim.api.nvim_get_current_buf(),
+		winnr = vim.api.nvim_get_current_win(),
+	}
+
+	-- Variables that setup can change
+	local result_table = {}
+	local builtin_list = vim.tbl_keys(builtin_pickers)
+	local extensions_list = vim.tbl_keys(extensions_pickers.manager)
+
+	for i, item in ipairs(builtin_list) do
+		table.insert(result_table, i, item)
+	end
+	for i, item in ipairs(extensions_list) do
+		table.insert(result_table, i, item)
+	end
+
+	pickers.new(opts_find_pickers or {}, {
 		prompt_title = "Find Pickers",
 		results_title = "Picker",
 		finder = finders.new_table({
@@ -47,15 +46,16 @@ M.find_pickers = function(opts)
 				local selection = actions_state.get_selected_entry()
 				local value = selection.value
 
+				actions._close(prompt_bufnr)
 				if builtin_pickers[value] ~= nil then
-					builtin_pickers[value]()
+					builtin_pickers[value](opts_pickers)
 				elseif extensions_pickers.manager[value] ~= nil then
-					extensions_pickers.manager[value][value]()
+					extensions_pickers.manager[value][value](opts_pickers)
 				end
 			end)
 			return true
 		end,
-		sorter = conf.file_sorter(opts),
+		sorter = conf.file_sorter(opts_find_pickers),
 	}):find()
 end
 
